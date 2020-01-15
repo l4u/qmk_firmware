3280d6cd0f4b68e0a40347d2234ee30c9166d4fc
illness072 <55215+illness072@users.noreply.github.com>
2020-01-15 18:50:13 +0900

    add elephant42 firmware

---
+++ keyboards/elephant42/keymaps/illness072/keymap.c
@@ -0,0 +1,177 @@
+
+/* Copyright 2020 illness072
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    QWERTY = SAFE_RANGE,
+    LOWER,
+    RAISE
+};
+
+
+#define KC_LOW LOWER
+#define KC_RAI RAISE
+#define KC_ KC_NO
+#define KC_____ KC_TRNS
+#define KC_XXXX KC_NO
+
+#define KC_LSPC LT(_LOWER, KC_SPC)
+#define KC_RSPC LT(_RAISE, KC_SPC)
+
+#define KC_MDEL CMD_T(KC_DEL)
+#define KC_CTAB CTL_T(KC_TAB)
+#define KC_SENT SFT_T(KC_ENT)
+#define KC_STAB SFT_T(KC_TAB)
+#define KC_ABSP ALT_T(KC_BSPC)
+
+#define KC_CDEL CTL_T(KC_DEL)
+
+
+#define KC_VU KC__VOLUP
+#define KC_VD KC__VOLDOWN
+#define KC_MU KC__MUTE
+
+#define KC_LTOG RGB_TOG
+#define KC_LMOF RGB_MOD
+#define KC_LMOP RGB_RMOD
+#define KC_LHUI RGB_HUI
+#define KC_LHUD RGB_HUD
+#define KC_LVAI RGB_VAI
+#define KC_LVAD RGB_VAD
+#define KC_LSAI RGB_SAI
+#define KC_LSAD RGB_SAD
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_kc( \
+  //,----+----+----+----+----+----.                        ,----+----+----+----+----+----.
+     TAB , Q  , W  , E  , R  , T  ,                          Y  , U  , I  , O  , P  ,MINS,
+  //|----+----+----+----+----+----|                        |----+----+----+----+----+----|
+     LCTL, A  , S  , D  , F  , G  ,                          H  , J  , K  , L  ,SCLN,QUOT,
+  //`----+----+----+----+----+----|                        |----+----+----+----+----+----'
+           Z  , X  , C  , V  , B  ,                          N  , M  ,COMM,DOT ,SLSH,
+  //     `----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----'
+                         LALT,LCMD,LSPC,LSFT,     CDEL,RSPC,SENT,ABSP
+  //                    `----+----+----+----'    `----+----+----+----'
+  ),
+
+  [_LOWER] = LAYOUT_kc(
+  //,----+----+----+----+----+----.                        ,----+----+----+----+----+----.
+     ESC , 1  , 2  , 3  , 4  , 5  ,                          6  , 7  , 8  , 9  , 0  ,EQL ,
+  //|----+----+----+----+----+----|                        |----+----+----+----+----+----|
+     LCTL,EXLM,AT  ,HASH,DLR ,PERC,                         CIRC,AMPR,ASTR,TILD,PLUS,GRV ,
+  //`----+----+----+----+----+----|                        |----+----+----+----+----+----'
+          PIPE,LABK,LCBR,LBRC,LPRN,                         RPRN,RBRC,RCBR,RABK,BSLS,
+  //     `----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----'
+                         LALT,LCMD,    ,LSFT,     CDEL,    ,SENT,ABSP
+  //                    `----+----+----+----'    `----+----+----+----'
+  ),
+
+  [_RAISE] = LAYOUT_kc(
+  //,----+----+----+----+----+----.                        ,----+----+----+----+----+----.
+         , F1 , F2 , F3 , F4 , F5 ,                          F6 , F7 , F8 , F9 ,F10 ,F11 ,
+  //|----+----+----+----+----+----|                        |----+----+----+----+----+----|
+         ,LTOG,LMOF,LVAI,LHUI,LSAI,                         LEFT,DOWN, UP ,RGHT,    ,F12 ,
+  //`----+----+----+----+----+----|                        |----+----+----+----+----+----'
+              ,LMOP,LVAD,LHUI,LSAD,                         HOME,PGDN,PGUP,END ,    ,
+  //     `----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----+----'
+                         LALT,LCMD,    ,LSFT,     CDEL,    ,SENT,ABSP
+  //                    `----+----+----+----'    `----+----+----+----'
+  )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+      } else {
+        layer_off(_LOWER);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+      } else {
+        layer_off(_RAISE);
+      }
+      return false;
+      break;
+    }
+    return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
+
+#ifdef OLED_DRIVER_ENABLE
+
+const char *read_logo(void) {
+  static char logo[] = {
+      0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+      0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+      0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+      0};
+  return logo;
+}
+
+void oled_task_user(void) {
+  if (is_keyboard_master()) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+    case _QWERTY:
+      oled_write_P(PSTR("Default\n"), false);
+      break;
+    case _LOWER:
+      oled_write_P(PSTR("Lower\n"), false);
+      break;
+    case _RAISE:
+      oled_write_P(PSTR("Raise\n"), false);
+      break;
+    default:
+      // Or use the write_ln shortcut over adding '\n' to the end of your string
+      oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    uint8_t led_usb_state = host_keyboard_leds();
+    oled_write_P(led_usb_state & (1<<USB_LED_NUM_LOCK) ? PSTR("NUMLCK ") : PSTR("       "), false);
+    oled_write_P(led_usb_state & (1<<USB_LED_CAPS_LOCK) ? PSTR("CAPLCK ") : PSTR("       "), false);
+    oled_write_P(led_usb_state & (1<<USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
+  } else {
+    oled_write(read_logo(), false);
+  }
+}
+#endif
---
+++ keyboards/elephant42/readme.md
@@ -0,0 +1,14 @@
+# elephant42
+
+My end-game keyboard.
+
+Keyboard Maintainer: [illness072](https://github.com/illness072)  
+Hardware Supported: None  
+Hardware Availability: None  
+
+Make example for this keyboard (after setting up your build environment):
+
+    make elephant42:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+# elephant42-firmware
